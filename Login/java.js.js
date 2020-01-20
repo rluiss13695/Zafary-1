50c8502dbc91e58c6aeacfbba6fa0ca9c610470a
rluiss13695 <rluiss13695@hotmail.com>
2020-01-20 17:59:15 -0300

    se agregaron index.html stylus.css y index.js

---
+++ Login/java.js.js
@@ -0,0 +1,11 @@
+function validacionUsuario(){
+    var username = document.getElementById("usuario").value;
+    var password = document.getElementById("contraseña").value;
+    if ( username == "ejemplo@gmail.com" && password == "123456"){
+        console.log ("Iniciando sesión…");
+        //window.location = "success.html"; 
+        return false;
+    }else{
+        console.log("Email y/o contraseña incorrectos");
+    }
+}
